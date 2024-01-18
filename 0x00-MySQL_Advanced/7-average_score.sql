838e34343bc5213b6eab7348f4ef9579f263eb3f
leila111111 <leila_merzak@outlook.fr>
2024-01-18 23:54:44 +0000

    tasks

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE new_score INT DEFAULT 0;
+    DECLARE new_score INT;
 
     SELECT AVG(score) INTO new_score FROM corrections WHERE user_id = in_user_id;
     UPDATE users SET average_score = new_score WHERE id = in_user_id;
