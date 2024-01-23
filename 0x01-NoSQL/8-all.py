af76f9dd5ed2bc8b6ddb24146f73ae13f5a3961c
leila111111 <leila_merzak@outlook.fr>
2024-01-23 20:22:34 +0000

    task8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Python function that lists all documents in a collection.
+'''
+
+
+def list_all(mongo_collection):
+    '''Python function that lists all documents in a collection
+    '''
+    return [doc for doc in mongo_collection.find()]
