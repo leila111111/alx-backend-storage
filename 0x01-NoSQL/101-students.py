e0b3fb50306ecdd7042ef0f8fcee3f9fddb636fe
leila111111 <leila_merzak@outlook.fr>
2024-01-23 20:57:20 +0000

    adv2

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Python function that returns all students sorted by average score:'''
+
+
+def top_students(mongo_collection):
+    '''Python function that returns all students sorted by average score:'''
+    liste = mongo_collection.aggregate([
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {'$avg': {'$avg': '$topics.score',},},
+                    'topics': 1,
+                }
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return liste
