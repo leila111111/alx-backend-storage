bf6b51470c36f9f0d566fc346bfeb189435500b9
leila111111 <leila_merzak@outlook.fr>
2024-01-23 20:44:27 +0000

    task12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Python script that provides some stats about Nginx logs stored in MongoDB:'''
+from pymongo import MongoClient
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_request_logs(client.logs.nginx)
+    print('{} logs'.format(client.logs.nginx.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        meth_count = client.logs.nginx.count_documents({"method": method})
+        print('\tmethod {}: {}'.format(method, meth_count))
+    count_stat = client.logs.nginx.count_documents({"method": "GET", "path": "/status"})
+    print('{} status check'.format(count_stat))
