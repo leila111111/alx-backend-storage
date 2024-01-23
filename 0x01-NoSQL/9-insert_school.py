ce7ffcf4a5f32bdf28fb3ccaa52e2877703f6435
leila111111 <leila_merzak@outlook.fr>
2024-01-23 20:25:14 +0000

    task9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Python function that inserts a new document in a collection based on kwargs'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''inserts a new document in a collection based on kwargs'''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
