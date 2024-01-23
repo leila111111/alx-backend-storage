d22c1b85c941a00d933f6f739a5c4fc4c5e3964a
leila111111 <leila_merzak@outlook.fr>
2024-01-23 20:48:35 +0000

    task12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,7 +4,6 @@ from pymongo import MongoClient
 
 if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
     print('{} logs'.format(client.logs.nginx.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
