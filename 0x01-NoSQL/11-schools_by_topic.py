45c1d6ef44310ba18fc994d3251b28872753f2d8
leila111111 <leila_merzak@outlook.fr>
2024-01-23 20:30:45 +0000

    task11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Python function that returns the list of school having a specific topic:"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """  Python function that returns the list of school having a specific topic:"""
+    topic = {'topics': topic}
+    return [doc for doc in mongo_collection.find(topic)]
