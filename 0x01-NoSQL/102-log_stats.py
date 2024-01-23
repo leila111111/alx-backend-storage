d400be72788facda88812958e22f9d5994a98edf
leila111111 <leila_merzak@outlook.fr>
2024-01-23 21:18:15 +0000

    adv3

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -25,6 +25,6 @@ if __name__ == '__main__':
             },
         ]
     )
-     print("IPs:")
+    print("IPs:")
     for log in count_log:
         print('\t{}: {}'.format(log['_id'], log['req']))
